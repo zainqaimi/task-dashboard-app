fbba97973db23b420c5c2fe852927405b9274ef7
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2026-02-26 07:05:05 +0500

    react vite and tailwind setup
    basic react router dom setup

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
